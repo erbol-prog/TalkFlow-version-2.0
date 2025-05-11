b1760344be2e77b0796fa0a9e60fe62085842427
erbol-prog <erbol.prog@gmail.com>
2025-05-11 15:21:55 +0600

    Updated socket cors origin

--- backend/ws_manager.py
+++ backend/ws_manager.py
@@ -3,10 +3,7 @@ import socketio
 # Shared Socket.IO server instance
 sio = socketio.AsyncServer(
     async_mode="asgi",
-    cors_allowed_origins=[
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-    ],  # Adjust origins as needed
+    cors_allowed_origins="*",
     async_handlers=True,
     ping_timeout=35000,
     logger=True,
